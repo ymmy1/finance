37ebd4c66b9c6ad660528ea2e3c2428f376a403a
ymmy1 <chem-pion@mail.ru>
2020-06-09 00:09:17 -0700

    database

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-DATABASE_URL = "sqlite:///finance.db"
+DATABASE_URL = SQL("sqlite:///finance.db")
 db = SQL("sqlite:///finance.db")
 # db = sqlite3.connect('finance.db')
 
