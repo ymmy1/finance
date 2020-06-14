e2a222bc638d0a3d1cfed2cf2db36d9e022741bf
ymmy1 <chem-pion@mail.ru>
2020-06-14 15:57:50 -0700

    heroku test

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import requests
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from tempfile import mkdtemp
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required, lookup, usd
