72ac6dd6b0b950f65f4379a0b793358d93908307
ymmy1 <chem-pion@mail.ru>
2020-06-15 18:09:16 -0700

    heroku

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
