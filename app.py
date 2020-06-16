605e746395e942c51658e1937d6cab7534d2c133
ymmy1 <chem-pion@mail.ru>
2020-06-15 20:56:41 -0700

    pipfileNew

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
-from tempfile import mkdtemp
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -26,7 +26,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -149,10 +148,12 @@ def history():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
+    """ Log user in """
 
     # Forget any user_id
     session.clear()
+
+    username = request.form.get("username")
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
@@ -164,21 +165,25 @@ def login():
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
-
-        # Query database for username
-        username=request.form.get("username")
-        rows = db.execute("SELECT * FROM users WHERE (username = :username)",
-                            {"username": username}).fetchall()
+            
+        # Query database for username (http://zetcode.com/db/sqlalchemy/rawsql/)
+        # https://docs.sqlalchemy.org/en/latest/core/connections.html#sqlalchemy.engine.ResultProxy
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                            {"username": username})
+        
+        result = rows.fetchone()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+        if result == None or not check_password_hash(result[2], request.form.get("password")):
+            return render_template("error.html", message="invalid username and/or password")
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = result[0]
+        session["user_name"] = result[1]
 
         # Redirect user to home page
         return redirect("/")
+
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
